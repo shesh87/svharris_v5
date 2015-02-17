85b917cd1d21c06613a40d999d50b7185290dfa7
Sheonna Harris <svonharris@gmail.com>
2015-02-17 17:50:05 -0500

    I've added this reponsive slider plugin and there is no instructions on how to use/activate the slider arrows in the documentation. Then of course on the support page the creater states I should ask stack overflow for implenmentation question, but I can't find any information on stack. Why are plugins so shittly documented.

--- public/js/responsiveslides.js
+++ public/js/responsiveslides.js
@@ -16,8 +16,8 @@
       "auto": false,             // Boolean: Animate automatically, true or false
       "speed": 500,             // Integer: Speed of the transition, in milliseconds
       "timeout": 4000,          // Integer: Time between slide transitions, in milliseconds
-      "pager": true,           // Boolean: Show pager, true or false
-      "nav": true,             // Boolean: Show navigation, true or false
+      "pager": false,           // Boolean: Show pager, true or false
+      "nav": false,             // Boolean: Show navigation, true or false
       "random": false,          // Boolean: Randomize the order of the slides, true or false
       "pause": false,           // Boolean: Pause on hover, true or false
       "pauseControls": true,    // Boolean: Pause when hovering controls, true or false
