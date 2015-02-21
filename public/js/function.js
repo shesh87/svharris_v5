9fa2d0ff782d5cf37faf54d8cd0a32974f8b57d1
Sheonna Harris <svonharris@gmail.com>
2015-02-21 09:10:07 -0500

    I reconfigured the mobile navigation to drop down instead of slide open. Not exactly what I wanted but it works for now.

--- public/js/function.js
+++ public/js/function.js
@@ -3,26 +3,44 @@ $('').on('hover', function() {
 })
 
 
+// $('.js-btn-nav').on('click touchstart', function() {
+// 	var clicks = $(this).data("clicks");
+// 	if (!clicks) {
+// 	    $('.js-move').animate({marginRight: "237px"}, 150);
+// 	    $('.js-main-contact').animate({
+// 		    right: 237
+// 		}, 150, function() {
+// 		    $('.js-main-contact').addClass('main-move');
+// 		});
+// 	} else {
+// 		$('.js-move').animate({marginRight: "0px"}, 150);
+// 		// $('.main-contact').animate({marginRight: "0"});
+// 	    $('.js-main-contact').animate({
+// 		    right: 0
+// 		}, 150, function() {
+// 		    $('.js-main-contact').removeClass('main-move');
+// 		});
+// 	}
+// 	    $(this).data("clicks", !clicks);
+// });
+
+
+
 $('.js-btn-nav').on('click touchstart', function() {
-	var clicks = $(this).data("clicks");
-	if (!clicks) {
-	    $('.js-move').animate({marginRight: "237px"}, 150);
-	    $('.js-main-contact').animate({
-		    right: 237
-		}, 150, function() {
-		    $('.js-main-contact').addClass('main-move');
-		});
-	    // {marginRight: "237px"}
-	      //second user click
-	} else {
-		$('.js-move').animate({marginRight: "0px"}, 150);
-		// $('.main-contact').animate({marginRight: "0"});
-	    $('.js-main-contact').animate({
-		    right: 0
-		}, 150, function() {
-		    $('.js-main-contact').removeClass('main-move');
-		});
-	}
-	    $(this).data("clicks", !clicks);
+	$('.js-navbar-sml').slideToggle();
 });
+
+if ('.navbar-sml')
+
+
+
+
+
+
+
+
+
+
+
+
 
