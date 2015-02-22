134315be69ca4d83e31f453a03be95e4a1c7a0ba
Sheonna Harris <svonharris@gmail.com>
2015-02-21 20:43:46 -0500

    Added website projects links & images.

--- public/js/function.js
+++ public/js/function.js
@@ -3,36 +3,20 @@ $('').on('hover', function() {
 })
 
 
-//MY ATTEMPT TO CREATE A SLIDING NAVIGATION. NOT RESPONSIVE READY THOUGH.
-// $('.js-btn-nav').on('click touchstart', function() {
-// 	var clicks = $(this).data("clicks");
-// 	if (!clicks) {
-// 	    $('.js-move').animate({marginRight: "237px"}, 150);
-// 	    $('.js-main-contact').animate({
-// 		    right: 237
-// 		}, 150, function() {
-// 		    $('.js-main-contact').addClass('main-move');
-// 		});
-// 	} else {
-// 		$('.js-move').animate({marginRight: "0px"}, 150);
-// 		// $('.main-contact').animate({marginRight: "0"});
-// 	    $('.js-main-contact').animate({
-// 		    right: 0
-// 		}, 150, function() {
-// 		    $('.js-main-contact').removeClass('main-move');
-// 		});
-// 	}
-// 	    $(this).data("clicks", !clicks);
-// });
-
-
-
 $('.js-btn-nav').on('click touchstart', function() {
 	$('.js-navbar-sml').slideToggle(350, 'linear');
 	$('.btn-nav.fa-bars').toggleClass('fa-bars-active');
 });
 
-// if ('.navbar-sml').
+function outsideClick() {
+	$('html').on('click touchstart', function() {
+		$('.js-navbar-sml').hide();
+		$('.btn-nav.fa-bars').removeClass('fa-bars-active');
+	});
+}
+	
+
+
 
 
 
