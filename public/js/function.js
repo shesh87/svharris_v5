acae069e578ab0f3074a51d59fb6b46cf507024d
Sheonna Harris <svonharris@gmail.com>
2015-02-26 16:17:54 -0500

    Finally finished the site.

--- public/js/function.js
+++ public/js/function.js
@@ -1,4 +1,3 @@
-
 $('.js-btn-nav').on('click', function() {
 	$('.js-navbar-sml').slideToggle(350, 'linear');
 	$('.btn-nav.fa-bars').toggleClass('fa-bars-active');
@@ -7,23 +6,13 @@ $('.js-btn-nav').on('click', function() {
 
 
 
-$(function () {
-
-  $("#slider1").responsiveSlides({
-    auto: true,
-    pager: true,
-    nav: true,
-    speed: 500,
-    // maxwidth: 800,
-    namespace: "centered-btns"
-  });
-
+$('.btn-up').on('click', function(event){
+  event.preventDefault();
+  $('body').animate({
+    scrollTop: 0,
+    }, 700
+  );
 });
-
-
-
-
-
 
 
 
