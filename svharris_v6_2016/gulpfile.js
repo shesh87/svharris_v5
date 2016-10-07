195e62f0818f4f2daf92a07c6a4e6272a283538a
Sheonna Harris <svonharris@gmail.com>
2016-10-07 14:55:32 -0400

    Installed gulp, compiled sass files and edited css.

---
+++ svharris_v6_2016/gulpfile.js
@@ -0,0 +1,17 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('default', function() {
+	console.log('hello world!');
+});
+
+gulp.task('styles', function() {
+	console.log("sass working..");
+	gulp.src('public/css/sass/all.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(autoprefixer({
+			browsers: ['last 2 versions']
+		}))
+		.pipe(gulp.dest('./public/css'));
+});
