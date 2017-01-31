0b2fd6f6f56b5daf1324c10671862ad4a8d9ebac
Sheonna Harris <svonharris@gmail.com>
2017-01-30 22:59:26 -0500

    input dummy data and returning it back to view

--- svharris_v6_2016/public/scripts/app.js
+++ svharris_v6_2016/public/scripts/app.js
@@ -137,136 +137,53 @@ app.factory("socialMediaService", function() {
 
 app.factory("projectService", function(linkService) {
 
-	var projects = [
-		{
-			category: "print",
-			thumbnail: "fab-tb.jpg",
-			alttext: "Fabulous Occasions Branding",
-			description: "Braning campaign for a event planning business.",
-			title: "Fabulous Occasions Branding",
-			company: "Fabulous Occasions, Inc.",
-			status: "College Student Project",
-			objective: "A one color design brochure project for a fictitious company turned into a full blown logo design and branding campaign for my mom's event planning aspirations.",
-			year: "2010",
-			tools: "InDesign, Photoshop, Illustrator, sketching",
-			photos: [{name: "fab-1.jpg", alttext: "Fabulous Occasions images"}, {name: "fab-1.jpg", alttext: "Fabulous Occasions images"}, {name: "fab-2.jpg", alttext: "Fabulous Occasions images"}, {name: "fab-3.jpg", alttext: "Fabulous Occasions images"}, {name: "fab-4.jpg", alttext: "Fabulous Occasions images"}, {name: "fab-5.jpg", alttext: "Fabulous Occasions images"}]
-		},
-		{
-			category: "web",
-			link: "web-proj-1.html",
-			thumbnail: "kingtut-tb.png",
-			alttext: "King Tut website",
-			title: "King Tut",
-			description: "A website redesign of an Orlando hookah design.",
-			company: "King Tut Hookah Lounge Orlando",
-			status: "College Student Project",
-			objective: "Choose a company website that was in need of a redesign. I've been a patrian of King Tut's and really enjoy the atmosphere they provide. My process was to design a website that had a middle eastern vibe that was modern and showcased their main product, hookahs.",
-			year: "2011",
-			tools: "Javascript, jQuery, Dreamweaver, HTML, CSS, Photoshop, Illustrator"
-		},
-		{
-			category: "web",
-			link: "web-proj-2.html",
-			thumbnail: "intoam-tb1.jpg",
-			alttext: "Into The AM website",
-			title: "Into The AM",
-			description: "An EDM merchandise and recording artist company."
-		},
-		{
-			category: "web",
-			link: "web-proj-3.html",
-			thumbnail: "npsbank-tb.jpg",
-			alttext: "NPSBANK website",
-			title: "NPSBANK",
-			description: "Small business credit card processor."
-		},
-		{
-			category: "mobile",
-			link: "mobile-proj-1.html",
-			thumbnail: "handson-tb.jpg",
-			alttext: "United Way HandsOn Suncoast mobile app",
-			title: "HandsOn",
-			description: "Non Profit United Way division in Tampa, FL."
-		},
-		{
-			category: "mobile",
-			link: "mobile-proj-2.html",
-			thumbnail: "spazio-tb.jpg",
-			alttext: "Spazio Italian Restaurant mobile app",
-			title: "Spazio",
-			description: "Restaurant on Fort Lauderdale Beach."
-		},
-		{
-			category: "mobile",
-			link: "mobile-proj-3.html",
-			thumbnail: "goldclub-tb.jpg",
-			alttext: "Gold Club adult club mobile app",
-			title: "Gold Club",
-			description: "Adult entertainment venue in San Francisco, CA."
-		},
-		{
-			category: "mobile",
-			link: "mobile-proj-4.html",
-			thumbnail: "rocresto-tb.jpg",
-			alttext: "ROC Resto Lounge mobile app",
-			title: "ROC Resto",
-			description: "Eat, drink, shop and dance all in one place."
-		},
-		{
-			category: 'code',
-			link: 'code-proj-4.html',
-			thumbnail: '',
-			alttext: '',
-			title: '',
-			description: ''
-		}
-	];
+	// var projects = [];
 
 
-	linkService.createLink(projects);
+	// linkService.createLink(projects);
 
-	function sections() {
-		var allProjects = [];
-		function addObj(obj, index) {
-			allProjects[index].contents.push(obj);
-		}
-		function createObj(obj, name) {
-			var newObj = {
-				category: name,
-				contents: [obj]
-			};
-			allProjects.push(newObj);
-		}
-		function findCate(obj) {
-			if (allProjects.length === 0) {
-				createObj(obj, obj.category);
-			} else {
-				var objCount = 0;
-				for (var i = 0; i < allProjects.length; i++) {
-					if (obj.category === allProjects[i].category) { // web.intoam === web.kingtut
-						objCount += 1;
-						var index = i;
-					}
-				}
-				if (objCount === 0) {
-					createObj(obj, obj.category);
-				} else {
-					addObj(obj, index);
+	var allProjects = [];
+	function addObj(obj, index) {
+		allProjects[index].contents.push(obj);
+	}
+	function createObj(obj, name) {
+		var newObj = {
+			category: name,
+			contents: [obj]
+		};
+		allProjects.push(newObj);
+	}
+	function findCate(obj) {
+		if (allProjects.length === 0) {
+			createObj(obj, obj.category);
+		} else {
+			var objCount = 0;
+			for (var i = 0; i < allProjects.length; i++) {
+				if (obj.category === allProjects[i].category) { // web.intoam === web.kingtut
+					objCount += 1;
+					var index = i;
 				}
 			}
+			if (objCount === 0) {
+				createObj(obj, obj.category);
+			} else {
+				addObj(obj, index);
+			}
 		}
+	}
+
+	function sections(projects) {
 		for (var i = 0; i < projects.length; i++) {
 			findCate(projects[i]);
 		}
-
 		return allProjects;
 	}	
 
 
 	return {
-		getSections: sections,
-		getAllProjects: function() {
-			return projects;
+		getAllProjects: function(array) {
+			linkService.createLink(array);
+			return sections(array);
 		}
 	};
 });
@@ -279,10 +196,10 @@ app.service('apiService', function($http) {
 			method: 'GET',
 			url: '/' + database
 		}).success(function(data){
-			// With the data succesfully returned, call our callback
+			// With the data successfully returned, call our callback
 			callbackFunc(data);
 		}).error(function(){
-			alert("error");
+			console.log("server call unsuccessful");
 		});
 	};
 });
@@ -304,13 +221,6 @@ app.factory('carrerService', function() {
 		return 0;
 	}
 
-	function convertDate(array) {
-		for (var i =0; i < array.length; i ++) {
-			Date.parse(array[i].timeline_start);
-			// map to new array
-		}
-	}
-
 	function getResume (array) {
 		for (var i =0; i < array.length; i ++) {
 			if (array[i].category === 'jobs') {
@@ -321,8 +231,6 @@ app.factory('carrerService', function() {
 				education.push(array[i]);
 			}
 		}
-
-		// convertDate(jobs);
 	}
 
 	
@@ -330,7 +238,7 @@ app.factory('carrerService', function() {
 	return {
 		getJobs: function(array) {
 			getResume(array);
-			// pastCareers.sort(compare);
+			jobs.sort(compare);
 			return jobs;
 		},
 		getSchools: function() {
@@ -341,8 +249,6 @@ app.factory('carrerService', function() {
 			return skills;
 		}
 	};
-
-
 });
 
 
@@ -389,6 +295,7 @@ app.controller('ProjectCtrl', function($scope, projectService, linkService) {
 
 
 app.controller('HeaderCtrl', function($scope, navigationService) {
+	$scope.navlinks = null;
 	$scope.navlinks = navigationService.getLinks();
 });
 
@@ -404,17 +311,11 @@ app.controller('BlogCtrl', function($scope, blogService, linkService) {
 });
 
 app.controller('ResumeCtrl', function($scope, carrerService, apiService) {
-	$scope.careers = null;
 	apiService.getData('career', function(response) {
-		// carrerService.getResume(response);
 		$scope.careers = carrerService.getJobs(response);
 		$scope.schools = carrerService.getSchools();
 		$scope.skills = carrerService.getSkills();
-		console.log(response);
 	});
-	
-	$scope.schools = carrerService.getSchools();
-	$scope.skills = carrerService.getSkills();
 	// $scope.getRating = function(rate) {
 	// 	var indexArray = [];
 	// 	for (var i=0; i < rate; i++) {
